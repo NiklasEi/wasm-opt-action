048e7d3df58fcde8fe5f98596ad3d8bd13713733
Niklas Eicker <git@nikl.me>
2022-03-26 11:48:34 +0100

    Keep order of optional params and document glob support (resolves #1)

--- Readme.md
+++ Readme.md
@@ -2,17 +2,31 @@
 
 This docker action allows usage of [binaryen's](https://github.com/WebAssembly/binaryen) wasm-opt to optimise Wasm files inside your GitHub workflows. Binaryen is available under its [Apache 2.0 License](https://github.com/WebAssembly/binaryen/blob/main/LICENSE).
 
+*Currently, this action uses Binaryen version 105*
+
 ## Usage
 
 In one of your GitHub workflow steps:
 ```yaml
       - name: Optimize Wasm
-        uses: NiklasEi/wasm-opt@v1
+        uses: NiklasEi/wasm-opt@v2
         with:
           file: some/path/to/file.wasm
           output: some/path/to/file.wasm
 ```
 
-The input parameters `file` and `output` are required. There is another optional parameter `options` with a default value of `-Os`. The input parameters are passed to `wasm-opt` like so: `<input> -o <output> <options>`. 
+The input parameter `file` is required. Unix shell like patterns are supported, but only the first match is optimized. The input parameter `output` is optional and defaults to the original file path. This is very useful if you pass a glob as `file`.
+
+For example, if your wasm files get a hash attached to them, you can optimize like this:
+```yaml
+      - name: Optimize Wasm
+        uses: NiklasEi/wasm-opt@v2
+        with:
+          file: dist/*.wasm
+```
+
+There is another optional parameter `options` with a default value of `-Os`. 
+
+The input parameters are passed to `wasm-opt` like so: `<input> -o <output> <options>`.
 
 Check the [wasm-opt options](https://github.com/WebAssembly/binaryen/blob/main/src/tools/optimization-options.h) for more info.
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,12 @@ use std::env;
 use std::process::Command;
 
 fn main() {
-    let mut args: Vec<String> = env::args().filter(|arg| arg.len() > 0).collect();
-    if args.len() < 2 {
+    // Github passes empty strings for not defined optional parameters.
+    // We have multiple optional params, so we need to keep their order.
+    let mut args: Vec<Option<String>> = env::args()
+        .map(|arg| if arg.len() > 0 { Some(arg) } else { None })
+        .collect();
+    if args.iter().filter(|arg| arg.is_some()).count() < 2 {
         panic!("You need to at least define a file to optimize");
     }
     args.remove(0);
@@ -13,7 +17,7 @@ fn main() {
 
     let glob_input = format!(
         "/github/workspace/{}",
-        args.pop().unwrap().trim_start_matches("/")
+        args.pop().unwrap().unwrap().trim_start_matches("/")
     );
     println!("Searching for file matching '{glob_input}'");
     let input = glob(&glob_input)
@@ -26,14 +30,15 @@ fn main() {
         .to_owned();
     println!("Optimizing '{input}'");
 
-    let output = if let Some(output) = args.pop() {
+    let output = if let Some(output) = args.pop().unwrap() {
         format!("/github/workspace/{}", output.trim_start_matches("/"))
     } else {
         input.clone()
     };
     println!("Writing optimized wasm file to '{output}'");
 
-    let options = args.first().unwrap_or(&"-Os".to_owned()).to_owned();
+    let options = args.pop().unwrap().unwrap_or("-Os".to_owned());
+    println!("Executing 'wasm-opt {input} -o {output} {options}'");
     Command::new("wasm-opt")
         .args([input, "-o".to_owned(), output, options])
         .output()

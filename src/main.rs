9edef62b3861fc80be17b3b2634418f3f617faa5
eickern <git@nikl.me>
2023-02-02 22:45:10 +0100

    Restructure and test argument parsing

--- src/main.rs
+++ src/main.rs
@@ -5,47 +5,141 @@ use std::process::Command;
 fn main() {
     // Github passes empty strings for not defined optional parameters.
     // We have multiple optional params, so we need to keep their order.
-    let mut args: Vec<Option<String>> = env::args()
+    let args = env::args()
         .map(|arg| if arg.len() > 0 { Some(arg) } else { None })
         .collect();
+    let config = parse(args, "/github/workspace/".to_owned());
+    run(config);
+}
+
+fn parse(mut args: Vec<Option<String>>, workspace: String) -> ActionConfig {
     if args.iter().filter(|arg| arg.is_some()).count() < 2 {
         panic!("You need to at least define a file to optimize");
     }
     args.remove(0);
     println!("Got arguments: {:?}", args);
     args.reverse();
-
     let glob_input = format!(
-        "/github/workspace/{}",
+        "{workspace}{}",
         args.pop().unwrap().unwrap().trim_start_matches("/")
     );
     println!("Searching for file matching '{glob_input}'");
-    let inputs = glob(&glob_input).expect("Failed to read glob pattern");
+    let mut files: Vec<String> = glob(&glob_input)
+        .expect("Failed to read glob pattern")
+        .map(|path| {
+            path.expect("Failed to read path")
+                .to_str()
+                .expect("Path should be string")
+                .to_owned()
+        })
+        .collect();
+
+    if files.is_empty() {
+        panic!("No file found matching '{glob_input}'");
+    }
+    let output = args
+        .pop()
+        .unwrap()
+        .map(|output| format!("/github/workspace/{}", output.trim_start_matches("/")));
     let optimize_all = args.pop().unwrap().unwrap_or("false".to_owned());
+    if optimize_all != "true" {
+        files.resize(1, String::new())
+    }
+    let options = args.pop().unwrap().unwrap_or("-Os".to_owned());
 
-    for path in inputs {
-        let input = path
-            .expect("Failed to read path")
-            .to_str()
-            .expect("Path should be string")
-            .to_owned();
+    ActionConfig {
+        files,
+        output,
+        options,
+    }
+}
+
+fn run(action: ActionConfig) {
+    let options = action.options;
+
+    for input in action.files {
+        let output = action.output.clone().unwrap_or(input.clone());
         println!("Optimizing '{input}'");
-
-        let output = if let Some(output) = args.pop().unwrap() {
-            format!("/github/workspace/{}", output.trim_start_matches("/"))
-        } else {
-            input.clone()
-        };
         println!("Writing optimized wasm file to '{output}'");
-
-        let options = args.pop().unwrap().unwrap_or("-Os".to_owned());
         println!("Executing 'wasm-opt {input} -o {output} {options}'");
         Command::new("wasm-opt")
-            .args([input, "-o".to_owned(), output, options])
+            .args([input, "-o".to_owned(), output.clone(), options.clone()])
             .output()
             .expect("failed to execute command");
-        if optimize_all != "true" {
-            break;
-        }
+    }
+}
+
+#[derive(PartialEq, Debug)]
+struct ActionConfig {
+    files: Vec<String>,
+    output: Option<String>,
+    options: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{parse, ActionConfig};
+
+    #[test]
+    fn single_file() {
+        assert_eq!(
+            parse(
+                vec![
+                    Some("executable".to_owned()),
+                    Some("*.md".to_owned()),
+                    None,
+                    None,
+                    None
+                ],
+                "./".to_owned()
+            ),
+            ActionConfig {
+                files: vec!["Deployment.md".to_owned()],
+                output: None,
+                options: "-Os".to_owned()
+            }
+        )
+    }
+
+    #[test]
+    fn multiple_files() {
+        assert_eq!(
+            parse(
+                vec![
+                    Some("executable".to_owned()),
+                    Some("*.md".to_owned()),
+                    None,
+                    Some("true".to_owned()),
+                    None
+                ],
+                "./".to_owned()
+            ),
+            ActionConfig {
+                files: vec!["Deployment.md".to_owned(), "Readme.md".to_owned()],
+                output: None,
+                options: "-Os".to_owned()
+            }
+        )
+    }
+
+    #[test]
+    fn custom_options() {
+        assert_eq!(
+            parse(
+                vec![
+                    Some("executable".to_owned()),
+                    Some("*.md".to_owned()),
+                    None,
+                    None,
+                    Some("custom".to_owned())
+                ],
+                "./".to_owned()
+            ),
+            ActionConfig {
+                files: vec!["Deployment.md".to_owned()],
+                output: None,
+                options: "custom".to_owned()
+            }
+        )
     }
 }

468b13e17288954dc13a0767e83cc2d6ff6a88c4
Niklas Eicker <git@nikl.me>
2023-08-07 10:52:44 +0200

    Use shell_words to properly split options

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use glob::glob;
-use std::{env, io};
 use std::io::Write;
 use std::process::Command;
+use std::{env, io};
 
 fn main() {
     // Github passes empty strings for not defined optional parameters.
     // We have multiple optional params, so we need to keep their order.
     let mut args: Vec<Option<String>> = env::args()
-        .map(|arg| if arg.len() > 0 { Some(arg) } else { None })
+        .map(|arg| if !arg.is_empty() { Some(arg) } else { None })
         .collect();
     if args.iter().filter(|arg| arg.is_some()).count() < 2 {
         panic!("You need to at least define a file to optimize");
@@ -18,7 +18,7 @@ fn main() {
 
     let glob_input = format!(
         "/github/workspace/{}",
-        args.pop().unwrap().unwrap().trim_start_matches("/")
+        args.pop().unwrap().unwrap().trim_start_matches('/')
     );
     println!("Searching for file matching '{glob_input}'");
     let inputs = glob(&glob_input).expect("Failed to read glob pattern");
@@ -33,7 +33,7 @@ fn main() {
         println!("Optimizing '{input}'");
 
         let output = if let Some(output) = args.pop().unwrap() {
-            format!("/github/workspace/{}", output.trim_start_matches("/"))
+            format!("/github/workspace/{}", output.trim_start_matches('/'))
         } else {
             input.clone()
         };
@@ -42,11 +42,16 @@ fn main() {
         let options = args.pop().unwrap().unwrap_or("-Os".to_owned());
         println!("Executing 'wasm-opt {input} -o {output} {options}'");
         let output = Command::new("wasm-opt")
-            .args([input, "-o".to_owned(), output, options])
+            .args([input, "-o".to_owned(), output])
+            .args(shell_words::split(&options).expect("Failed to parse options"))
             .output()
             .expect("failed to execute command");
-        io::stdout().write_all(&output.stdout).expect("failed to write to stdout");
-        io::stderr().write_all(&output.stderr).expect("failed to write to stderr");
+        io::stdout()
+            .write_all(&output.stdout)
+            .expect("failed to write to stdout");
+        io::stderr()
+            .write_all(&output.stderr)
+            .expect("failed to write to stderr");
         if optimize_all != "true" {
             break;
         }

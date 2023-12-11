e21a595e5a41ac0070f536e62a019c0407b05c60
Niklas Eicker <git@nikl.me>
2023-12-11 14:46:56 +0100

    Test log

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ fn main() -> Result<(), io::Error> {
     }
     args.remove(0);
     println!("Got arguments: {:?}", args);
+    println!("testing");
     args.reverse();
 
     let glob_input = format!(
